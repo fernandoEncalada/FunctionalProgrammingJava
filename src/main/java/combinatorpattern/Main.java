1520ba92a84e00ca81ed89fee7e2932b5277b9f8
fernandoEncalada <ferencalada1@gmail.com>
2024-04-29 23:10:54 -0500

    add features

--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -2,11 +2,28 @@ package combinatorpattern;
 
 import java.time.LocalDate;
 
+import static combinatorpattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer("Fernando", "fer@gmail.com", "+0158748464", LocalDate.of(1999, 6, 2));
+        Customer customer = new Customer("Fernando", "fergmail.com", "+0158748464", LocalDate.of(1999, 6, 2));
 
         System.out.println(new CustomerValidatorService().isValid(customer));
+
+        // if is valid we can save the customer in the DB
+
+        // Using combinator pattern
+        ValidationResult result =
+                isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
 
     }
 }
